ecf232676a4e19ebfac36a18a3478d999db6c82b
Eric <leagues58@gmail.com>
2019-06-08 16:08:44 -0400

    Rewrote everything. Installed socketio and got working with test msgs

--- app.js
+++ app.js
@@ -1,53 +1,30 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const port = 4040;
 
-var app = express();
-const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+})
 
-// view engine setup
+const io = require('socket.io').listen(server);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render(
-    'index',{}
-  );
-});
+  res.render('index', { title: 'group b' });
+})
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+io.on('connection', (socket) => {
+  socket.on('chatter', function(message) {
+    console.log('message : ' + message);
+  });
+  socket.on('test', (data) => {
+    console.log(data)
+  });
+  socket.emit('sendToClient', 'hello, client');
 });
