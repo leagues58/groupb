451ace1f00fc6d5f413a8774890709123e7523c1
Eric <leagues58@gmail.com>
2019-06-09 20:18:29 -0400

    more rewrites. connect to db now

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const sql = require('mssql');
 
 const port = 4040;
+
+const config = {
+  user: 'SA',
+  password: '<YourStrong!Passw0rd>',
+  server: 'localhost',
+  database: 'SpellingBee'
+};
+
+sql.connect(config).catch(err => console.log(err));
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 })
 
 const io = require('socket.io').listen(server);
+const index = require('./routes/index.js');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'group b' });
-})
+app.use('/', index);
 
 io.on('connection', (socket) => {
   socket.on('chatter', function(message) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+const sql = require('mssql');
+var fs = require('fs');
+
+var test;
+
+const fileName = './../puzzle.json';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+    (async function query(){
+        // get puzzle from db
+        const request = new sql.Request();
+        const result = await request.query(`
+            SELECT 
+                * 
+            FROM Puzzle 
+            INNER JOIN Answer 
+                ON Answer.PuzzleId = Puzzle.Id
+            WHERE
+                CONVERT (date, Puzzle.DateCreated) = CONVERT (date, GETDATE())
+            ORDER BY 
+                Puzzle.DateCreated DESC
+        `)
+
+        // if no puzzle in db, get from file
+        if(result.recordset.length == 0) {
+            fs.stat(fileName, function(err, stat) {
+                if(err == null) {
+                    fs.readFile(fileName, 'utf8', function (err, data) {
+                        if (err) throw err;
+                        console.log('reading file');
+                        let resulttext = JSON.parse(data);
+                    });      
+                }
+            });
+        }
+
+        res.render('index', { title: 'groub b', puzzle: result });
+    })();
     
-    res.render('index', { title: 'groub b' });
   });
-  
+
   module.exports = router;
