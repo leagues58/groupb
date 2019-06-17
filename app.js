7b585949367953229384791d3c3a52320b49501d
Eric <leagues58@gmail.com>
2019-06-16 22:23:28 -0400

    More style work, rewrote word checking logic to make more sense

--- app.js
+++ app.js
@@ -30,20 +30,21 @@ app.io = io;
 //app.use('/', index);
 app.get('/', function(req, res, next) {
 
-  res.render('index', { title: 'groub bee', gameData: '', test: 'hello', foundWords: foundWords });
+  res.render('index', { title: 'groub bee', gameData: puzzle, test: 'hello', foundWords: foundWords, totalWordsCount: puzzle.today.answers.length });
 });
 
 io.on('connection', (socket) => {
+
   socket.on('guess', (guess) => {
-    console.log(`a client just guessed: ${guess}`);
-  
-  puzzle.today.answers.forEach(element => {
-      if (guess.toLowerCase() == element) {
-          if (foundWords.indexOf(guess) == -1) {
-            io.emit('answerFound', guess);
-            foundWords.push(guess);
-          }     
+    console.log(`client ${socket.id} just guessed: ${guess}`);  
+    for (let word of puzzle.today.answers) {
+      if (guess.toLowerCase() == word) {
+        if (foundWords.indexOf(guess) == -1) {
+          foundWords.push(guess);
+          io.emit('answerFound', {answer: guess, foundWordsCount: foundWords.length});
+          break;
+        }     
       }
-  });
+    }
   });
 });
